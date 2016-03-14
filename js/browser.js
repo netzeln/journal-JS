d23e5d382787e96e8e8e0819acaa455194338a4a
jessica fix and nic netzel <netzeln@gmail.com>
2016-03-14 11:34:49 -0700

    Add wordCount function and display on index.html

--- js/browser.js
+++ js/browser.js
@@ -7,13 +7,12 @@ $(document).ready(function() {
     var title = $('input#title').val();
     var text = $('input#text').val();
     var date = $('input#date').val();
-    console.log(title);
-    console.log(text);
-    console.log(date);
+
+
 
     var newEntry = new Entry(title, text, date);
-
-      $('.entries').append("<p><strong>" + newEntry.title + "</strong><br><em>" + newEntry.bodyText + "</em><br>" + newEntry.date + "</p>");
+    var words = newEntry.countWords();
+      $('.entries').append("<p><strong>" + newEntry.title + "</strong><br><em>" + newEntry.bodyText + "</em><br>" + newEntry.date + "<br>" + words + " words in this entry</p>");
 
   });
 });
