1948d791cfa3e9ffd15413a27fa5bff1ddb394c3
jessica fix and nic netzel <netzeln@gmail.com>
2016-03-14 09:47:23 -0700

    Add prompt for date

--- js/cli.js
+++ js/cli.js
@@ -4,12 +4,14 @@ prompt.start();
 
 // var entry = new Entry();
 
-prompt.get(['title','text'], function(err, result) {
+prompt.get(['title','text', 'date'], function(err, result) {
   var entry = new Entry();
   entry.title = result.title;
   entry.bodyText = result.text;
+  entry.date = result.date;
   console.log(entry.title);
-  console.log(entry.bodyText)
+  console.log(entry.bodyText);
+  console.log(entry.date);
 });
 
 // prompt.get('what should we say', function(err, result2) {
