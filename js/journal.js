d0a3d68a15e5135b27ebc01ce59924c469fd8720
jessica fix and nic netzel <netzeln@gmail.com>
2016-03-14 11:56:25 -0700

    Add jshint package and fix semicolins

--- js/journal.js
+++ js/journal.js
@@ -2,8 +2,8 @@ exports.Entry = function (title, bodyText, date) {
   this.title = title;
   this.bodyText = bodyText;
   this.date = date;
-}
+};
 
 exports.Entry.prototype.countWords = function(){
   return this.bodyText.split(" ").length;
-}
+};
