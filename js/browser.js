d6fd1607ebb702ae8989b9353258cd98ccb70036
jessica fix and nic netzel <jcfix.pdx@gmail.com>
2016-03-14 10:20:20 -0700

    Add jquery to append entries

--- js/browser.js
+++ js/browser.js
@@ -1,5 +1,16 @@
-$(document).ready(function(event) {
+$(document).ready(function() {
 
-
+$('#new-entry').submit(function(event){
   event.preventDefault();
+  var title = $('#title').val();
+  var text = $('#text').val();
+  var date = $('#date').val();
+
+  var newEntry = new Entry(title, text, date);
+  newEntry.forEach(function(element) {
+    $('.entries').append("<strong>" + element.title + "</strong><br><em>" + element.date + "</em><br>" + element.bodyText);
+  });
+});
+
+
 });
