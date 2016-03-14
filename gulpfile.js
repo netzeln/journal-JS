52c04a5d4f81d77f3e0d1269fbb013fa55f6ceed
jessica fix and nic netzel <netzeln@gmail.com>
2016-03-14 11:42:40 -0700

    Minify app.js with gulp uglify package

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var uglify = require('gulp-uglify');
 
 gulp.task ('jsBrowserify', function(){
   return browserify({entries: ['./js/browser.js'] })
     .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('minifyScripts', ['jsBrowserify'], function() {
+  return gulp.src('./build/js/app.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('./build/js'));
 });
--- js/browser.js
+++ js/browser.js
@@ -8,8 +8,6 @@ $(document).ready(function() {
     var text = $('input#text').val();
     var date = $('input#date').val();
 
-
-
     var newEntry = new Entry(title, text, date);
     var words = newEntry.countWords();
       $('.entries').append("<p><strong>" + newEntry.title + "</strong><br><em>" + newEntry.bodyText + "</em><br>" + newEntry.date + "<br>" + words + " words in this entry</p>");
